920cb10dc9a3b2e6e03efbd7856db5e538135e1d
Barracuda Team <admin@omega8.cc>
2016-02-06 15:48:16 +0100

    Sync comments

--- lib/Drush/Boot/DrupalBoot.php
+++ lib/Drush/Boot/DrupalBoot.php
@@ -112,9 +112,9 @@ abstract class DrupalBoot extends BaseBoot {
     switch ($phase) {
       case DRUSH_BOOTSTRAP_DRUPAL_ROOT:
         $drupal_root = drush_get_context('DRUSH_SELECTED_DRUPAL_ROOT');
+        // BOA restriction
         // $searchpath[] = $drupal_root . '/../drush';
         // $searchpath[] = $drupal_root . '/drush';
-        // BOA restriction: Use only Aegir managed in sites/all/drush
         $searchpath[] = $drupal_root . '/sites/all/drush';
         break;
       case DRUSH_BOOTSTRAP_DRUPAL_SITE:
