5fc85e1902dec4e0b307c95983326243d3bbc7f9
BOA Dev Team <boa@omega8.cc>
2023-05-16 12:06:33 +0200

    Disable open_basedir check

--- src/Preflight/PreflightVerify.php
+++ src/Preflight/PreflightVerify.php
@@ -61,7 +61,7 @@ class PreflightVerify
      */
     protected function checkPhpIni()
     {
-        $ini_checks = ['safe_mode' => '', 'open_basedir' => ''];
+        $ini_checks = ['safe_mode' => '', 'x_open_basedir' => ''];
 
         // Test to insure that certain php ini restrictions have not been enabled
         $prohibited_list = [];
